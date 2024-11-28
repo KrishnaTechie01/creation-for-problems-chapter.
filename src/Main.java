9f7bd79f621bc1506ce1715af97d4a9e811e9182
Shree Krishna <krishna.mathiyalagan01@gmail.com>
2024-11-28 22:08:51 +0530

    init commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class  Main{
+    //method
+    public static void main(String[] args){
+        System.out.println("enter input:");
+        Scanner scan = new Scanner(System.in);
+        int num = scan.nextInt();
+        System.out.println(num);
+    }
+
+}
