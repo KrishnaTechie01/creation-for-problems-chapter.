cdbf4ea5eb3f2d82471644cf4c040cef2d8732ee
Shree Krishna <krishna.mathiyalagan01@gmail.com>
2024-12-01 05:49:02 +0000

    init commit

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,17 @@
 import java.util.Scanner;
+import java.lang.String;
 class  Main{
     //method
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println("enter input:");
-        Scanner scan = new Scanner(System.in);
-        int num = scan.nextInt();
-        System.out.println(num);
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        sc.nextLine();
+        String m = sc.nextLine();
+        char c = sc.next().charAt(0);
+        System.out.println(a);
+        System.out.println(m);
+        System.out.println(c);
     }
 
 }
