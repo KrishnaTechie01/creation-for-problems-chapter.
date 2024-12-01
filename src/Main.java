3cd291bd288454b7286b43018f537ca404209f4e
Shree Krishna <krishna.mathiyalagan01@gmail.com>
2024-12-01 06:11:11 +0000

    init commit

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,18 @@
 import java.util.Scanner;
-import java.lang.String;
-class  Main{
-    //method
-    public static void main(String[] args) {
-        System.out.println("enter input:");
+class Main{
+    public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the values of the angles:");
         int a = sc.nextInt();
-        sc.nextLine();
-        String m = sc.nextLine();
-        char c = sc.next().charAt(0);
-        System.out.println(a);
-        System.out.println(m);
-        System.out.println(c);
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        if( a + b + c == 180){
+             System.out.println("Triangle can be Formed");
+        }
+        else{
+            System.out.println("IT's Can't be Formed");
+        }
+
     }
 
 }
